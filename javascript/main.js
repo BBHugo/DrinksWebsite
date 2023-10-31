6bea5b50e1614b4a83a3b21f0e2c7be8d395f524
Hugo Mendoza <thehugomendoza@gmail.com>
2023-10-30 21:03:35 -0700

    Pseudo Code

--- javascript/main.js
+++ javascript/main.js
@@ -7,7 +7,9 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
         alert(`error ${err}`)
     })
 
-
+//We still need to get the path to the images and the description/recipe.
+//Then we have to use the `ticks` to make it a dynamic search through the input bar and button.
+//Lastly it'd be better if we use the arrays given to us from the JSON to make a list we could click on.
 
 document.querySelector("button").addEventListener("click", getDrink)
 
