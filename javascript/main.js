a32db2c9cb0899907c97b577054a0c62f4983b8c
Hugo Mendoza <thehugomendoza@gmail.com>
2023-11-02 17:56:41 -0700

    updated the basics of the API to website

--- javascript/main.js
+++ javascript/main.js
@@ -1,33 +1,35 @@
 fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-
+        // console.log(data)
+        // console.log(data.drinks)
+        console.log(data.drinks[0])
     })
     .catch(err => {
         alert(`error ${err}`)
     })
 
+//The code below is supposed to try to split the information given in the chosen array, find the ingredients and list them. But so far idk how to split the information within the API's array.
 
-document.querySelector("h2").addEventListener("click",test)
+// document.querySelector("h2").addEventListener("click",test)
 
-function test(){
-    // console.log(data.drinks[0].includes('strIngredient'))
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=random')
-    .then(res => res.json())    
-    .then(data => {
-    data.drinks[0].forEach((x) => {
-        if(x.includes('strIngredient')){
-            console.log(x)
-        }else{
-            console.log('This aint working')
-        }
-    })
-    })
-    .catch(err => {
-        alert(`error ${err}`)
-    })
-}
+// function test(){
+//     // console.log(data.drinks[0].includes('strIngredient'))
+//     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+//     .then(res => res.json())    
+//     .then(data => {
+//     // data.drinks[0].toArray().forEach((x) => {
+//     //     if(x.includes('strIngredient') && !x.includes('null')){
+//     //         console.log(x)
+//     //     }else{
+//     //         console.log('This aint working')
+//     //     }
+//     // })
+//     })
+//     .catch(err => {
+//         alert(`error ${err}`)
+//     })
+// }
 
 
     // data.drinks[0].forEach((x,i) => {
@@ -51,14 +53,17 @@ function getDrink(){
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        document.querySelector("img").src = data.drinks[0].strImageSource
-/* THE STUFF BELOW IS TRYING TO GET ALL THE INGREDIENTS FROM THE API ARRAY TO PUT THEM IN ORDER */   
-        data.drinks[0].forEach((x,i) => {
-            if(includes('strIngredient')){
-                document.querySelector(".ingredients").innerText = data.drinks[i].strIngredient
-            }
-        })
+        document.querySelector("img").src = data.drinks[0].strDrinkThumb
+        document.querySelector('.instructions').innerText = data.drinks[0].strInstructions
+/****************** THE STUFF BELOW IS TRYING TO GET ALL THE INGREDIENTS FROM THE API ARRAY TO PUT THEM IN ORDER********************** */   
+        // data.drinks[0].forEach((x,i) => {
+        //     if(includes('strIngredient')){
+        //         document.querySelector(".ingredients").innerText = data.drinks[i].strIngredient
+        //     }
+        // })
         // document.querySelector(".ingredients")
+
+/*************************************************************************************************************************************** */
     })
     .catch(err => {
         alert(`error ${err}`)
